009fbbeaa9f9403f6f67befc909f566b83d874d7
Victor Svensson <80740775+victorsvensson@users.noreply.github.com>
2023-02-12 23:13:52 +0100

    Sum

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
-import { getWeather } from "./weather"
-import "./styles.css"
+import { getWeather } from "./weather";
+import "./styles.css";
 
-navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
 
-function positionSuccess({coords}){
-  getWeather(coords.latitude, coords.longitude, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderCurrentWeather).catch(e => {
-    console.error(e)
-    alert("Error getting weather")
-  })
+function positionSuccess({ coords }) {
+    getWeather(
+        coords.latitude,
+        coords.longitude,
+        Intl.DateTimeFormat().resolvedOptions().timeZone
+    )
+        .then(renderCurrentWeather)
+        .catch((e) => {
+            console.error(e);
+            alert("Error getting weather");
+        });
 }
 
-function positionError(){
-  alert("There was an error getting your location. Please allow us to use your location and refresh the page")
+function positionError() {
+    alert(
+        "There was an error getting your location. Please allow us to use your location and refresh the page"
+    );
 }
 
-let currentCity = document.querySelector("[data-current-city]")
-let currentTemperature = document.querySelector("[data-current-temp]")
-let currentFeelsLike = document.querySelector("[data-current-feelsLike]")
-let currentHighTemp = document.querySelector("[data-current-highTemp]")
-let currentLowTemp = document.querySelector("[data-current-lowTemp]")
-let currentHumidity = document.querySelector("[data-current-humidity]")
+let currentCity = document.querySelector("[data-current-city]");
+let currentTemperature = document.querySelector("[data-current-temp]");
+let currentFeelsLike = document.querySelector("[data-current-feelsLike]");
+let currentHighTemp = document.querySelector("[data-current-highTemp]");
+let currentLowTemp = document.querySelector("[data-current-lowTemp]");
+let currentHumidity = document.querySelector("[data-current-humidity]");
 
-
-function renderCurrentWeather(current_weather){
-
-  // currentCity.innerHTML = "City: " + current_weather.name;
-  currentTemperature.innerHTML = Math.round(current_weather.main.temp) + "&deg";
-  // currentFeelsLike.innerHTML = "Feels like: " + current_weather.main.feels_like + "&deg";
-  currentHighTemp.innerHTML = "Highest: " + Math.round(current_weather.main.temp_max) + "&deg";
-  currentLowTemp.innerHTML = "Lowest: " + Math.round(current_weather.main.temp_min) + "&deg";
-  currentHumidity.innerHTML = "Humidity: " + Math.round(current_weather.main.humidity) + "%";
-  SetCurrentWeatherIcon(current_weather);
+function renderCurrentWeather(current_weather) {
+    // currentCity.innerHTML = "City: " + current_weather.name;
+    currentTemperature.innerHTML =
+        Math.round(current_weather.main.temp) + "&deg";
+    // currentFeelsLike.innerHTML = "Feels like: " + current_weather.main.feels_like + "&deg";
+    currentHighTemp.innerHTML =
+        "Highest: " + Math.round(current_weather.main.temp_max) + "&deg";
+    currentLowTemp.innerHTML =
+        "Lowest: " + Math.round(current_weather.main.temp_min) + "&deg";
+    currentHumidity.innerHTML =
+        "Humidity: " + Math.round(current_weather.main.humidity) + "%";
+    SetCurrentWeatherIcon(current_weather);
 }
 
-let currentWeatherIcon = document.querySelector("[data-current-weather-icon]")
+let currentWeatherIcon = document.querySelector("[data-current-weather-icon]");
 
-function SetCurrentWeatherIcon(current_weather){
-  currentWeatherIcon.src = `https://openweathermap.org/img/wn/${current_weather.weather[0].icon}@2x.png`;
-
+function SetCurrentWeatherIcon(current_weather) {
+    currentWeatherIcon.src = "public/images/sun.png";
 }
-
-
